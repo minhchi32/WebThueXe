2c56d0e874a536e1e5027c5d9f5235e8a7496fd4
minhchi32 <tranminhchi0302@gmail.com>
2021-04-27 15:11:18 +0700

    Add project files.

---
+++ WebThueXe/Controllers/KeToanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebThueXe.Controllers
+{
+    public class KeToanController : Controller
+    {
+        // GET: KeToan
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebThueXe/Controllers/XeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebThueXe.Controllers
+{
+    public class XeController : Controller
+    {
+        // GET: Xe
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ChiTietXe(int id=0)
+        {
+            return View();
+        }
+        public ActionResult HopDongThueXe()
+        {
+            return View();
+        }
+    }
+}
