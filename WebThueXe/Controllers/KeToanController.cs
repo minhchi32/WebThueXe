7e7ec06e7574ac75bc98c64702803de08bc14ca2
minhchi32 <tranminhchi0302@gmail.com>
2021-04-30 22:45:11 +0700

    manage car list

--- WebThueXe/Controllers/KeToanController.cs
+++ WebThueXe/Controllers/KeToanController.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebThueXe.Models;
 
 namespace WebThueXe.Controllers
 {
     public class KeToanController : Controller
     {
+        DBCarRentalEntities database = new DBCarRentalEntities();
         // GET: KeToan
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult QuanLyXe()
+        {
+            return View(database.Xes.ToList());
         }
     }
 }
