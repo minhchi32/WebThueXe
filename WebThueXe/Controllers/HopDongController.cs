5e200cb8734122de2bb4655dea6d8b16e937032d
tuankiet1709 <tranhatuankiet1709@gmail.com>
2021-05-11 06:38:14 +0700

    sua 1 chut

---
+++ WebThueXe/Controllers/HopDongController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebThueXe.Models;
+
+namespace WebThueXe.Controllers
+{
+    public class HopDongController : Controller
+    {
+        DBCarRentalEntities db = new DBCarRentalEntities();
+        // GET: HopDong
+        public ActionResult Index()
+        {
+            return View(db.HopDongs.ToList());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Create_Detail()
+        {
+            return View();
+        }
+    }
+}
